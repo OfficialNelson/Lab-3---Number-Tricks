62710274ffca55b27f7a7214377b4e0cbf7bb695
OfficialNelson <officialnelsonkan@gmail.com>
2023-09-16 14:14:28 -0400

    changed format of js

--- MagicNumber.js
+++ MagicNumber.js
@@ -1,4 +1,3 @@
-// JavaScript comments with name and date
 // Created by Nelson Kan 
 // Date: Sep 14, 2023
 
@@ -15,11 +14,11 @@ var finalNumber = step4 - userNumber;
 // Display the output using document.write
 document.write("<p>You chose the number: " + userNumber + "</p>");
 document.write("<ul>");
-document.write("<li>I added 9: " + step1 + "</li>");
-document.write("<li>I multiplied by 2: " + step2 + "</li>");
-document.write("<li>I subtracted by 4: " + step3 + "</li>");
-document.write("<li>I divided by 2: " + step4 + "</li>");
-document.write("<li>I subtracted by the original number: " + finalNumber + "</li>");
+document.write("<li>I added 9 to " + userNumber + ". The new number is " + step1 + ".</li>");
+document.write("<li>I multiplied " + step1 + " by 2. The new number is " + step2 + ".</li>");
+document.write("<li>I subtracted 4 from " + step2 + ". The new number is " + step3 + ".</li>");
+document.write("<li>I divided " + step3 + " by 2. The new number is " + step4 + ".</li>");
+document.write("<li>I subtracted " + userNumber + " by the original number, which is " + finalNumber + ".</li>");
 document.write("</ul>");
 
 // Display the final result using an alert
